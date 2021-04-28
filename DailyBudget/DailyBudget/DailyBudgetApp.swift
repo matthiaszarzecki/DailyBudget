f6c6b23060750241aa3817fb957feafb8c1a6011
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-28 15:19:21 +0200

    Add empty project

---
+++ DailyBudget/DailyBudget/DailyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBudgetApp.swift
+//  DailyBudget
+//
+//  Created by Matthias Zarzecki on 28.04.21.
+//
+
+import SwiftUI
+
+@main
+struct DailyBudgetApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
